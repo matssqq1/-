dc511d268de4196f405ee234a629e190bfaba1d2
Avairon <lipsknaz@gmail.com>
2024-12-01 21:05:27 +0700

    modified:   api/get_info.py 	modified:   api/test_api.py 	new file:   cache/09.07.32.html 	new file:   data/search_data.json

--- api/get_info.py
+++ api/get_info.py
@@ -1,5 +1,13 @@
+import requests
+import os
 class parcer:
+    
+
     def __init__(self):
         pass
     def get_info(self, group, date):
-        pass
+        url = 'https://расписание.нхтк.рф/%s.html#заголовок' % (group) # url для второй страницы
+        r = requests.get(url)
+        
+        with open('cache/%s.html' % (group), 'w') as output_file:
+            output_file.write(r.text.encode('cp1251'))
--- api/test_api.py
+++ api/test_api.py
@@ -1,2 +1,4 @@
 import get_info
 
+getter = get_info.parcer()
+getter.get_info("09.07.32", "02.12.2024")
