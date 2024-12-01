7aee181f299a973895f6a2cca10cd9c0c698b96a
Avairon <lipsknaz@gmail.com>
2024-12-01 21:40:37 +0700

    modified:   api/get_info.py 	modified:   cache/09.07.32.html

--- api/get_info.py
+++ api/get_info.py
@@ -1,13 +1,20 @@
 import requests
-import os
+import codecs
 class parcer:
     
 
     def __init__(self):
         pass
     def get_info(self, group, date):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+        }
         url = 'https://расписание.нхтк.рф/%s.html#заголовок' % (group) # url для второй страницы
-        r = requests.get(url)
-        
-        with open('cache/%s.html' % (group), 'w') as output_file:
-            output_file.write(r.text.encode('cp1251'))
+        response = requests.get(url=url, headers=headers)
+        response.encoding = response.apparent_encoding
+
+        print(response.text)
+
+        out_file = codecs.open('cache/%s.html' % (group), 'w', 'utf-8')
+        out_file.write(response.text)
+        out_file.close()
