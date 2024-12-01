aef48c2ca00a9c87aac2bd6456d2deb721cf1114
mats <matveimin@yandex.ru>
2024-12-01 11:20:08 +0700

    updates

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot('8130108661:AAES_8zx0x-fWAucFfUQ7GHTmFqV_WES5k4')
-#начало
-#создаём калву с кнопками
+
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-services = types.KeyboardButton("Services")
+services = types.KeyboardButton("1")
 menu.add(services)
-#создаем кнопку назад
+
 back = types.ReplyKeyboardMarkup(resize_keyboard=True)
 back_button = types.KeyboardButton("Back")
 back.add(back_button)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "hellos!", reply_markup = menu)
+    bot.send_message(message.chat.id, "hello!", reply_markup = menu)
 
-bot.infinity_polling() #конец бота
+bot.infinity_polling()
