c29bacc1b2b6959486d56622a698c40d33f0bca4
Avairon <lipsknaz@gmail.com>
2024-12-02 00:10:48 +0700

    modified:   api/test_api.py

--- api/test_api.py
+++ api/test_api.py
@@ -8,14 +8,14 @@ import datetime
 # 4 - prepod
 # 5 - room
 
-date = datetime.date(2024, 12, 2)
+date = datetime.date(2024, 12, 2) # date for get info
 
-getter = get_info.parcer()
+getter = get_info.parcer() # create object for parcing(maybe i create other methods)
 
-if getter.get_info("09.07.32", date) == 0:
+if getter.get_info("09.07.32", date) == 0: # check for errors and creating json
 
-    with open('cache/09.07.32.json', 'r') as file:
+    with open('cache/09.07.32.json', 'r') as file: # read the json
         data = json.load(file)
 
-    for i in range(len(data)):
+    for i in range(len(data)): # print the json
         print(data[i])
