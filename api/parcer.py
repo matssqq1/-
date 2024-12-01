c7af955e83d8a2c4d559c56b6cdb262053bcffd2
Avairon <lipsknaz@gmail.com>
2024-12-02 03:37:36 +0700

    modified:   README.md 	modified:   api/parcer.py 	deleted:    api/test_api.py 	modified:   data/users.json 	new file:   logs/errors.log 	new file:   logs/user_changes.log 	modified:   main.py

--- api/parcer.py
+++ api/parcer.py
@@ -5,157 +5,187 @@ import json
 import re
 import os
 
-class parcer:
+def log_write(log_name, text):
+    file = open(log_name, 'a')
+    file.write(text + '\n')
+    file.close()
     
-
+class parcer:
     def __init__(self):
         pass
     def get_info(self, group, date):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
-        }
-        url = 'https://расписание.нхтк.рф/%s.html#заголовок' % (group) # url для второй страницы
-        response = requests.get(url=url, headers=headers)
-        response.encoding = response.apparent_encoding
-
-        #print(response.text)
-
-        out_file = codecs.open('cache/%s.html' % (group), 'w', 'utf-8')
-        out_file.write(response.text)
-        out_file.close()
-
-        html_file = codecs.open('cache/%s.html' % (group), 'r', 'utf-8')
-        soup = BeautifulSoup(html_file.read(), 'html.parser')
-        html_file.close()
-        
-        os.remove('cache/%s.html' % (group))
-
-        month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
-        days_list = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-
-        date_call = "%s, %s %s" % (days_list[date.weekday()], date.day, month_list[date.month - 1]) # date to format: Суббота, 7 декабря
-
-        delete_list = ['\xad', '\n', '\xa0']
-        change_list = ['', '|', ' ']
-
-        day = soup.select("tr")
-
-        result_rows = []
-        out_rows = []
-        para = []
-
-        for stroke in day:
-            s = stroke.text
-
-            for i in range(len(delete_list)):
-                s = s.replace(delete_list[i], change_list[i])
-            
-            result_rows.append(s)
-        
-        search_state = False
-
-        for i in range(len(result_rows)): # base HTML to list parcing
-            if result_rows[i] == date_call:
-                search_state = True
-                i += 1
-
-            if search_state == True and result_rows[i] != 'ВремяПредметПреподавательПрепод.Ауд.':
-                out_rows.append(result_rows[i])
-            
-            if result_rows[i].split(',')[0] in days_list and search_state == True:
-                search_state = False
-
-        #print(out_rows)
-
-        for i in range(len(out_rows)): # final list parcing
-            for j in range(len(out_rows[i].split('|'))):
-                buff = out_rows[i].split('|')[j]
-
-                if j == 1: # time parcing
-                    para.append(buff[:1])
-
-                    buff = buff[1:]
-                    if len(buff) > 21:
-                        buff = buff[:len(buff) - 11]
-                    else:
-                        buff = buff[:len(buff) - 10]
-                    para.append(buff.split(' ')[0])
-                    continue
-
-                if buff != '':
-                    para.append(buff)
         try:
-            para.pop(len(para) - 1)
-        except:
-            return -1
-
-        # print(para[1])
-        # print(para[1].split('–')[1])
-
-        objects = [] # json write
-        for i in range(0, len(para), 5):
-            obj = {
-                "number": para[i],
-                "time-start": para[i+1].split('–')[0],
-                "time-end": para[i+1].split('–')[1],
-                "subject": para[i+2],
-                "teacher": para[i+3],
-                "room": para[i+4]
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
             }
-            objects.append(obj)
+            url = 'https://расписание.нхтк.рф/%s.html#заголовок' % (group) # url для второй страницы
+            response = requests.get(url=url, headers=headers)
+            response.encoding = response.apparent_encoding
 
-        # objects to json
-        json_string = json.dumps(objects, ensure_ascii=False)
+            #print(response.text)
 
-        json_file = open('cache/%s.json' % (group), 'w')
-        json_file.write(json_string)
-        json_file.close()
+            out_file = codecs.open('cache/%s.html' % (group), 'w', 'utf-8')
+            out_file.write(response.text)
+            out_file.close()
 
-        return 0
+            html_file = codecs.open('cache/%s.html' % (group), 'r', 'utf-8')
+            soup = BeautifulSoup(html_file.read(), 'html.parser')
+            html_file.close()
+
+            os.remove('cache/%s.html' % (group))
+
+            month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+            days_list = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+
+            date_call = "%s, %s %s" % (days_list[date.weekday()], date.day, month_list[date.month - 1]) # date to format: Суббота, 7 декабря
+
+            delete_list = ['\xad', '\n', '\xa0']
+            change_list = ['', '|', ' ']
+
+            day = soup.select("tr")
+
+            result_rows = []
+            out_rows = []
+            para = []
+
+            for stroke in day:
+                s = stroke.text
+
+                for i in range(len(delete_list)):
+                    s = s.replace(delete_list[i], change_list[i])
+
+                result_rows.append(s)
+
+            search_state = False
+
+            for i in range(len(result_rows)): # base HTML to list parcing
+                if result_rows[i] == date_call:
+                    search_state = True
+                    i += 1
+
+                if search_state == True and result_rows[i] != 'ВремяПредметПреподавательПрепод.Ауд.':
+                    out_rows.append(result_rows[i])
+
+                if result_rows[i].split(',')[0] in days_list and search_state == True:
+                    search_state = False
+
+            #print(out_rows)
+
+            for i in range(len(out_rows)): # final list parcing
+                for j in range(len(out_rows[i].split('|'))):
+                    buff = out_rows[i].split('|')[j]
+
+                    if j == 1: # time parcing
+                        para.append(buff[:1])
+
+                        buff = buff[1:]
+                        if len(buff) > 21:
+                            buff = buff[:len(buff) - 11]
+                        else:
+                            buff = buff[:len(buff) - 10]
+                        para.append(buff.split(' ')[0])
+                        continue
+
+                    if buff != '':
+                        para.append(buff)
+            try:
+                para.pop(len(para) - 1)
+            except:
+                return -1
+
+            # print(para[1])
+            # print(para[1].split('–')[1])
+
+            objects = [] # json write
+            for i in range(0, len(para), 5):
+                obj = {
+                    "number": para[i],
+                    "time-start": para[i+1].split('–')[0],
+                    "time-end": para[i+1].split('–')[1],
+                    "subject": para[i+2],
+                    "teacher": para[i+3],
+                    "room": para[i+4]
+                }
+                objects.append(obj)
+
+            # objects to json
+            json_string = json.dumps(objects, ensure_ascii=False)
+
+            json_file = open('cache/%s.json' % (group), 'w')
+            json_file.write(json_string)
+            json_file.close()
+
+            return 0
+        except Exception as e:
+            log_write("logs/errors.log", str(e))
     
     
     def read_users(chat_id):
-        with open('data/users.json', 'r') as file: # read the json
-            data = json.load(file)
+        try:
+            with open('data/users.json', 'r') as file: # read the json
+                data = json.load(file)
 
-        for i in range(len(data)):
-            #print(data[i])
-            if data[i]['chat_id'] == chat_id:
-                return data[i]['group']
-        
-        return -1
+            for i in range(len(data)):
+                #print(data[i])
+                if data[i]['chat_id'] == chat_id:
+                    return data[i]['group']
+
+            return -1
+
+        except Exception as e:
+            log_write("logs/errors.log", str(e))
     
     def write_users(self, chat_id, group):
-        with open('data/users.json', 'r') as file: # read the json
-            data = json.load(file)
-        
-        statement = True
-        
-        group = group.strip()
+        try:
+            with open('data/users.json', 'r') as file: # read the json
+                data = json.load(file)
 
-        pattern = r'^\d{2}.\d{2}.\d{2}$'
-        #print(group)
+            statement = True
 
-        # Проверяем соответствие
-        if re.match(pattern, group):
-            pass
-        else:
-            return -1
-        
-        for i in range(len(data)):
-            #print(data[i])
-            if data[i]['chat_id'] == chat_id:
-                if data[i]['group'] != group:
-                    data[i]['group'] = group
+            group = group.strip()
+
+            pattern = r'^\d{2}.\d{2}.\d{2}$'
+            #print(group)
+
+            # Проверяем соответствие
+            if re.match(pattern, group):
+                pass
+            else:
+                log_write('logs/user_changes.log', f"uncorrect transaction occured! user: {chat_id}, group: {group}")
+                return -1
+
+            for i in range(len(data)):
+                #print(data[i])
+                if data[i]['chat_id'] == chat_id:
+                    if data[i]['group'] != group:
+                        data[i]['group'] = group
+
+                        log_write('logs/user_changes.log', f"user: {chat_id} change group to: {group}")
+
+                        statement = False
+                        break
                     statement = False
                     break
-                statement = False
-                break
 
-        if statement:
-            data.append({"chat_id": chat_id, "group": group})
+            if statement:
+                data.append({"chat_id": chat_id, "group": group})
+                log_write('logs/user_changes.log', f"user: {chat_id} create profile, group: {group}")
 
-        file = open('data/users.json', 'w')
-        content = json.dumps(data)
-        file.write(content)
-        file.close()
+            file = open('data/users.json', 'w')
+            content = json.dumps(data)
+            file.write(content)
+            file.close()
+        except Exception as e:
+            log_write("logs/errors.log", str(e))
+        
+    
+    def json_read(self, json_name):
+        try:
+            with open(json_name, 'r') as file: # read the json
+                data = json.load(file)
+            os.remove(json_name)
+
+            return data
+        except Exception as e:
+            log_write("logs/errors.log", str(e))
+    
--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import telebot
 from telebot import types
-from src.test import taims
+# from src.test import taims
 from api.parcer import parcer
 import datetime
-import json
-import os
+# import json
 
-# dates and months
+# dates and months for printing data
 month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 days_list = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 
 
 #вытаскиваем из масива часы и менуты для бота
-a = taims()
-start_hous = a[0]
-start_minets = a[1]
-ends_hous = a[2]
-ends_minets = a[3]
+# a = taims()
+# start_hous = a[0]
+# start_minets = a[1]
+# ends_hous = a[2]
+# ends_minets = a[3]
 
 bot = telebot.TeleBot('8130108661:AAES_8zx0x-fWAucFfUQ7GHTmFqV_WES5k4')
 
 #end calculation command body/органа управления расчетами
-menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
 #para_ends = types.KeyboardButton("Время до каонца текущей пары")
 #para_start = types.KeyboardButton("время до начала следующей пары")
 
-menu.add(types.KeyboardButton("Пара"), types.KeyboardButton("Группа"))
-
-back = types.ReplyKeyboardMarkup(resize_keyboard=True)
-back_button = types.KeyboardButton("аим ис кам бек кхе кхе..")
-back.add(back_button)
+menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menu.add(types.KeyboardButton("Пара"), types.KeyboardButton("Группа")) # add 2 buttons
+ 
+# back = types.ReplyKeyboardMarkup(resize_keyboard=True)
+# back_button = types.KeyboardButton("аим ис кам бек кхе кхе..")
+# back.add(back_button)
 
 getter = parcer()
 #start bot codding commands /start
@@ -47,85 +47,88 @@ def message(message):
 
 @bot.message_handler(content_types=['text'])
 def text_messages(message):
-    start_hous_bot = start_hous
-    start_minets_bot = start_minets
-    ends_hous_bot = ends_hous
-    ends_minets_bot = ends_minets
+    # start_hous_bot = start_hous
+    # start_minets_bot = start_minets
+    # ends_hous_bot = ends_hous
+    # ends_minets_bot = ends_minets
+    try:
+        if message.text[:4].lower() == 'пара': # check for command пара
+            global change_group_flag # flag init
+            change_group_flag = False
 
-    if message.text[:4].lower() == 'пара':
-        today = datetime.datetime.now()
-        #today = today.replace(day=8, hour=0, minute=11) # DEBUG
-        days_ago = 0
+            today = datetime.datetime.now()
+            #today = today.replace(day=8, hour=0, minute=11) # DEBUG
+            days_ago = 0
 
-        group = parcer.read_users(message.chat.id)
-        if group == -1:
-            bot.send_message(message.chat.id, "У вас нет группы! Напишите команду Группа", reply_markup=menu)
-            return
+            group = parcer.read_users(message.chat.id)
+            if group == -1:
+                bot.send_message(message.chat.id, "У вас нет группы! Напишите команду Группа", reply_markup=menu)
+                return
 
-        for i in range(5):
-            if getter.get_info(group, today) == 0:
-                with open('cache/%s.json' % group, 'r') as file:
-                    data = json.load(file)
-                
-                os.remove('cache/%s.json' % (group))
+            for i in range(5): # check the last 5 days
+                if getter.get_info(group, today) == 0:
 
-                date_call = "%s, %s %s" % (days_list[today.weekday()], today.day, month_list[today.month - 1])
+                    data = getter.json_read(f'cache/{group}.json') # read the json
 
-                para_time = today
-                
+                    date_call = "%s, %s %s" % (days_list[today.weekday()], today.day, month_list[today.month - 1]) # date to format: Суббота, 7 декабря
 
-                para_ends_today = today
-                para_ends_today = para_ends_today.replace(hour=int(data[len(data) - 1]['time-end'].split(':')[0]), minute=int(data[len(data) - 1]['time-end'].split(':')[1]))
-                
-                
+                    para_time = today # write to para_time date
 
-                if para_ends_today < today and para_ends_today.day == today.day:
-                    bot.send_message(message.chat.id, f"Сегодня пар нет! Последняя пара закончилась в: {data[len(data) - 1]['time-end']}", reply_markup=menu)
+                    # OLD VERSION FEATURE
+                    #
+                    # para_ends_today = today # write to para_ends_today date
+                    # para_ends_today = para_ends_today.replace(hour=int(data[len(data) - 1]['time-end'].split(':')[0]), minute=int(data[len(data) - 1]['time-end'].split(':')[1]))
+                    # 
+                    # 
+                    # 
+                    # if para_ends_today < today and para_ends_today.day == today.day: # if 
+                    #     bot.send_message(message.chat.id, f"Сегодня пар нет! Последняя пара закончилась в: {data[len(data) - 1]['time-end']}", reply_markup=menu)
+                    # 
+                    #     today = today + datetime.timedelta(days=1)
+                    #     today = today.replace(hour=0, minute=0)
 
+                    for i in range(len(data)):
+                        para_time = para_time.replace(hour=int(data[i]['time-start'].split(':')[0]), minute=int(data[i]['time-start'].split(':')[1])) # write to para_time starts   time
+
+                        delta = para_time - today # calculate delta
+
+                        if para_time > today: # check if nearest para
+                            if days_ago > 0: # check todsy is para
+                                bot.send_message(message.chat.id, f"Ближайшая пара через: {days_ago} дн\n{date_call}\n{data[i]['number']}, пройдёт: {data[i]['time-start']} - {data [i]['time-end']},\n{data[i]['subject']} {data[i]['teacher']} {data[i]['room']}", reply_markup=menu)
+                            else:
+                                bot.send_message(message.chat.id, f"Ближайшая пара через: {str(delta)[:5].split(':')[0]}ч {str(delta)[:5].split(':')[1]}м\n{date_call}\n{data[i]    ['number']}, пройдёт: {data[i]['time-start']} - {data[i]['time-end']},\n{data[i]['subject']} {data[i]['teacher']} {data[i]['room']}",   reply_markup=menu)
+                            return
+
+                else: # if today is no para
+                    days_ago = days_ago + 1
                     today = today + datetime.timedelta(days=1)
                     today = today.replace(hour=0, minute=0)
 
-                for i in range(len(data)):
-                    para_time = para_time.replace(hour=int(data[i]['time-start'].split(':')[0]), minute=int(data[i]['time-start'].split(':')[1]))
+            bot.send_message(message.chat.id, "В ближайшее время нет пар!", reply_markup=menu) # if no para in nearest 5 days
 
-                    delta = para_time - today
+        elif message.text[:6].lower() == 'группа': # группа command
+            bot.send_message(message.chat.id, "А теперь напиши свою группу!", reply_markup=menu)
+            change_group_flag = True
 
-                    if para_time > today:
-                        if days_ago > 0:
-                            bot.send_message(message.chat.id, f"Ближайшая пара через: {days_ago} дн\n{date_call}\n{data[i]['number']}, пройдёт: {data[i]['time-start']} - {data[i]['time-end']},\n{data[i]['subject']} {data[i]['teacher']} {data[i]['room']}", reply_markup=menu)
-                        else:
-                            bot.send_message(message.chat.id, f"Ближайшая пара через: {str(delta)[:5].split(':')[0]}ч {str(delta)[:5].split(':')[1]}м\n{date_call}\n{data[i]['number']}, пройдёт: {data[i]['time-start']} - {data[i]['time-end']},\n{data[i]['subject']} {data[i]['teacher']} {data[i]['room']}", reply_markup=menu)
-                        return
-                
-            else:
-                days_ago = days_ago + 1
-                today = today + datetime.timedelta(days=1)
-                today = today.replace(hour=0, minute=0)
+        elif change_group_flag == True: # change group command
+            group = message.text
+            if getter.write_users(message.chat.id, group) == -1: # try to write and check if all good
+                bot.send_message(message.chat.id, "Неккоректный формат группы, попробуйте еще раз! Формат в виде: 00.00.00", reply_markup=menu)
+                return
 
-        bot.send_message(message.chat.id, "в ближайшее время нет пар!", reply_markup=menu)
+            bot.send_message(message.chat.id, "Группа сохранена", reply_markup=menu)
+            #global change_group_flag
+            change_group_flag = False
 
-    elif message.text[:6].lower() == 'группа':
-        bot.send_message(message.chat.id, "а теперь напиши свою группу!", reply_markup=menu)
-        global change_group_flag
-        change_group_flag = True
-        
-    elif change_group_flag == True:
-        group = message.text
-        if getter.write_users(message.chat.id, group) == -1:
-            bot.send_message(message.chat.id, "Неккоректный формат группы, попробуйте еще раз! Формат в виде: 00.00.00", reply_markup=menu)
-            return
-        
-        bot.send_message(message.chat.id, "группа сохранена", reply_markup=menu)
-        #global change_group_flag
-        change_group_flag = False
-
-    # elif message.text == 'аим ис кам бек кхе кхе..':
-    #     bot.send_message(message.chat.id, "Добро пожаловать обратно сударь каково ваше следующие желание", reply_markup=menu)
-    # elif message.text == 'время до начала следующей пары':
-    #     bot.send_message(message.chat.id, f"{start_hous_bot}:{start_minets_bot}", reply_markup=back)
-    # elif message.text == 'Время до каонца текущей пары':
-    #     bot.send_message(message.chat.id, f"{ends_hous_bot}:{ends_minets_bot}", reply_markup=back)
-    else:
-        bot.send_message(message.chat.id, "некорректная команда", reply_markup=menu)
+        # elif message.text == 'аим ис кам бек кхе кхе..':
+        #     bot.send_message(message.chat.id, "Добро пожаловать обратно сударь каково ваше следующие желание", reply_markup=menu)
+        # elif message.text == 'время до начала следующей пары':
+        #     bot.send_message(message.chat.id, f"{start_hous_bot}:{start_minets_bot}", reply_markup=back)
+        # elif message.text == 'Время до каонца текущей пары':
+        #     bot.send_message(message.chat.id, f"{ends_hous_bot}:{ends_minets_bot}", reply_markup=back)
+        else: # if uncorrect command
+            bot.send_message(message.chat.id, "некорректная команда", reply_markup=menu)
+    except Exception as e:
+        parcer.log_write('logs/errors.log', str(e))
 
 bot.infinity_polling()
